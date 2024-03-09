fad07da7bec2d407e90823852f076877ef8cbb77
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-09 23:13:24 +0100

    Getting sections is an 1 argument method, moves file

--- Loaders/camcadLoader.py
+++ Loaders/camcadLoader.py
@@ -3,16 +3,16 @@ class CamCadLoader:
         self.filePath = filePath
         self.boardData = {}
         self.sectionsLineNumbers = {'BOARDINFO':[], 'PARTLIST':[], 'PNDATA':[], 'NETLIST':[], 'PAD':[], 'PACKAGES':[], 'BOARDOUTLINE':[]}
-        self._getSectionsLinesBeginEnd()
+        
+        fileLines = self._getFileLines()
+        self._getSectionsLinesBeginEnd(fileLines)
     
     def _getFileLines(self) -> list[str]:
         with open(self.filePath, 'r') as file:
             fileLines = file.readlines()
         return fileLines
 
-    def _getSectionsLinesBeginEnd(self):
-        fileLines = self._getFileLines()
-        
+    def _getSectionsLinesBeginEnd(self, fileLines):
         for i, line in enumerate(fileLines):
             sectionName = line[1:-1]
             if sectionName in self.sectionsLineNumbers or (sectionName:=sectionName[3:]) in self.sectionsLineNumbers:
