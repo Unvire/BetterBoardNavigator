7ae7508f6e6f9e77422a05614894812893a1bf6d
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-09 23:00:18 +0100

    Moves file to loaders folder

---
+++ Loaders/camcadLoader.py
@@ -0,0 +1,25 @@
+class CamCadLoader:
+    def __init__(self, filePath:str):
+        self.filePath = filePath
+        self.boardData = {}
+        self.sectionsLineNumbers = {'BOARDINFO':[], 'PARTLIST':[], 'PNDATA':[], 'NETLIST':[], 'PAD':[], 'PACKAGES':[], 'BOARDOUTLINE':[]}
+        self._getSectionsLinesBeginEnd()
+    
+    def _getFileLines(self) -> list[str]:
+        with open(self.filePath, 'r') as file:
+            fileLines = file.readlines()
+        return fileLines
+
+    def _getSectionsLinesBeginEnd(self):
+        fileLines = self._getFileLines()
+        
+        for i, line in enumerate(fileLines):
+            sectionName = line[1:-1]
+            if sectionName in self.sectionsLineNumbers or (sectionName:=sectionName[3:]) in self.sectionsLineNumbers:
+                self.sectionsLineNumbers[sectionName].append(i)
+    
+
+
+if __name__ == '__main__':
+    loader = CamCadLoader(r'C:\Users\krzys\Documents\GitHub\boardNavigator\Schematic\lvm Core.cad')
+    print(loader.sectionsLineNumbers)
