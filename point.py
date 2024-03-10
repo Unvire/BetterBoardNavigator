ed1788e06a1e15ff447c147be3ccfdbb6e260aa1
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-10 19:12:59 +0100

    Inits point class for storing coordinates and operations on them

--- point.py
+++ point.py
@@ -2,3 +2,6 @@ class Point:
     def __init__(self, x:float|int, y:float|int):
         self.x = x
         self.y = y
+    
+    def __eq__(self, point:'Point'):
+        return self.x == point.x and self.y == point.y
