eec71a6b73e50089a29f6581b1b89e056ff74a67
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-12 23:09:51 +0100

    Adds test for line comparison

--- Tests/test_geometryObjects.py
+++ Tests/test_geometryObjects.py
@@ -12,3 +12,13 @@ def test_Pointrepr():
     pointB = geometryObjects.Point(1.2, 1.2)
     repr = pointB.__str__()
     assert repr == 'Point x=1.2, y=1.2'
+ 
+def test_LineEqual():
+    pointA = geometryObjects.Point(0, 0)
+    pointB = geometryObjects.Point(1.2, 1.2)
+    pointC = geometryObjects.Point(1, -1)
+    line1 = geometryObjects.Line(pointA, pointB)
+    line2 = geometryObjects.Line(pointB, pointA)
+    line3 = geometryObjects.Line(pointC, pointA)
+    assert line1 == line2
+    assert line1 != line3
