7d1faf9b26000a39a4aa088e1e882e7c41475441
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-12 23:05:17 +0100

    Changes name of the tests that are related to point class

--- Tests/test_geometryObjects.py
+++ Tests/test_geometryObjects.py
@@ -1,14 +1,14 @@
 import pytest
 import geometryObjects
 
-def test_equal():
+def test_Pointequal():
     pointA = geometryObjects.Point(0, 0)
     pointB = geometryObjects.Point(1.2, 1.2)
     pointC = geometryObjects.Point(1.2, 1.2)
     assert pointA != pointB
     assert pointB == pointC
 
-def test_repr():
+def test_Pointrepr():
     pointB = geometryObjects.Point(1.2, 1.2)
     repr = pointB.__str__()
     assert repr == 'Point x=1.2, y=1.2'
