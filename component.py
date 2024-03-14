0d1518afa28fa269ba1e6375be7484fbdaef3763
Unvire <krzysztof.balcerzak6@gmail.com>
2024-03-14 12:14:36 +0100

    Adds component class

---
+++ component.py
@@ -0,0 +1,22 @@
+import geometryObjects
+
+class Component:
+    def __init__(self, name:str):
+        self.name = name
+        self.pins = []
+        self.coords = None
+        self.side = None
+        self.angle = 0
+        self.package = None
+
+    def addPin(self, pin:str, point:geometryObjects.Point):
+        self.pins.append([pin, point])
+    
+    def setCoords(self, point:geometryObjects.Point):
+        self.coords = point
+    
+    def setSide(self, side:str):
+        self.side = side
+
+    def setAngle(self, angle:float):
+        self.angle = angle
