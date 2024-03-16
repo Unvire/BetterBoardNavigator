c10ddf2efdf28fa391e49538537e1153645641ee
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-16 16:52:35 +0100

    Adds tests for calculating data from pins

--- Tests/test_component.py
+++ Tests/test_component.py
@@ -2,6 +2,15 @@ import pytest
 import component
 import geometryObjects
 
+@pytest.fixture
+def componentForPinsCalculation():
+    testedComponent = component.Component('1')
+    testedComponent.addPin('1', geometryObjects.Point(-1, -1), 'net1')
+    testedComponent.addPin('2', geometryObjects.Point(3, 2), 'net1')
+    testedComponent.addPin('3', geometryObjects.Point(-1, 1), 'net1')
+    testedComponent.addPin('4', geometryObjects.Point(2, 0), 'net1')
+    return testedComponent
+
 @pytest.mark.parametrize('input, expected', [((None, None), False), ((2, 3), True), ((None, 2), False)])
 def test_isCoordsValid(input, expected):
     x, y = input
@@ -10,11 +19,17 @@ def test_isCoordsValid(input, expected):
     testedComponent.setCoords(point)
     assert testedComponent.isCoordsValid() == expected
 
-def test_calculateCenterFromPins():
-    testedComponent = component.Component('1')
-    testedComponent.addPin('1', geometryObjects.Point(-1, -1), 'net1')
-    testedComponent.addPin('2', geometryObjects.Point(3, 2), 'net1')
-    testedComponent.addPin('3', geometryObjects.Point(-1, 1), 'net1')
-    testedComponent.addPin('4', geometryObjects.Point(2, 0), 'net1')
-    testedComponent.calculateCenterFromPins()
-    assert testedComponent.coords.x == 1 and testedComponent.coords.y == 0.5
+def test_calculateHitBoxFromPins(componentForPinsCalculation):
+    point1, point2 = componentForPinsCalculation.calculateHitBoxFromPins()
+    assert point1 == geometryObjects.Point(-1, -1)
+    assert point2 == geometryObjects.Point(3, 2)
+
+def test_calculateCenterFromPins(componentForPinsCalculation):    
+    componentForPinsCalculation.calculateCenterFromPins()
+    assert componentForPinsCalculation.coords.x == 1 and componentForPinsCalculation.coords.y == 0.5
+
+def test_calculatePackageFromPins(componentForPinsCalculation):
+    componentForPinsCalculation.calculatePackageFromPins()
+    point1, point2 = componentForPinsCalculation.package
+    assert point1 == geometryObjects.Point(-0.95, -0.95)
+    assert point2 == geometryObjects.Point(2.85, 1.9)
