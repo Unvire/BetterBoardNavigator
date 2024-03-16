3bb1cfaa0b8b7954fc6a0e0408e9bd6880ae7057
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-16 15:58:35 +0100

    Adds test for test_calculateCenterFromPins

--- Tests/test_component.py
+++ Tests/test_component.py
@@ -9,3 +9,12 @@ def test_isCoordsValid(input, expected):
     testedComponent = component.Component('1')
     testedComponent.setCoords(point)
     assert testedComponent.isCoordsValid() == expected
+
+def test_calculateCenterFromPins():
+    testedComponent = component.Component('1')
+    testedComponent.addPin('1', geometryObjects.Point(-1, -1), 'net1')
+    testedComponent.addPin('2', geometryObjects.Point(3, 2), 'net1')
+    testedComponent.addPin('3', geometryObjects.Point(-1, 1), 'net1')
+    testedComponent.addPin('4', geometryObjects.Point(2, 0), 'net1')
+    testedComponent.calculateCenterFromPins()
+    assert testedComponent.coords.x == 1 and testedComponent.coords.y == 0.5
