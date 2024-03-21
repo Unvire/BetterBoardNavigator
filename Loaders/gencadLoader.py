0c6764f113af0404625b760cba5339ded367ba9a
Unvire <krzysztof.balcerzak6@gmail.com>
2024-03-21 15:45:51 +0100

    Inits gencad loader

---
+++ Loaders/gencadLoader.py
@@ -0,0 +1,34 @@
+import sys, os, copy 
+sys.path.append(os.getcwd())
+import geometryObjects
+import board, component, pin
+
+class GenCadLoader:
+    def __init__(self):
+        self.boardData = board.Board()
+        self.sectionsLineNumbers = {'BOARD':[], 'PADS':[], 'SHAPES':[], 'COMPONENTS':[], 'SIGNALS':[], 'ROUTES':[], 'MECH':[]}
+    
+    def loadFile(self, filePath:str):
+        self._setFilePath(filePath)
+        fileLines = self._getFileLines()        
+        self._getSectionsLinesBeginEnd(fileLines)
+
+        return self.boardData
+    
+    def _setFilePath(self, filePath:str):
+        self.filePath = filePath
+    
+    def _getFileLines(self) -> list[str]:
+        with open(self.filePath, 'r') as file:
+            fileLines = file.readlines()
+        return fileLines
+    
+    def _getSectionsLinesBeginEnd(self, fileLines:str):
+        for i, line in enumerate(fileLines):
+            sectionName = line[1:-1]
+            if sectionName in self.sectionsLineNumbers or (sectionName:=sectionName[3:]) in self.sectionsLineNumbers:
+                self.sectionsLineNumbers[sectionName].append(i)
+
+if __name__ == '__main__':
+    loader = GenCadLoader()
+    loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\wallbox main rev5.GCD')
