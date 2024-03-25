a91552cfeea397802420c63e22aed178eb2518af
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-25 19:31:50 +0100

    Adds methods that extract board outlines and board area from BOARD section

--- Loaders/gencadLoader.py
+++ Loaders/gencadLoader.py
@@ -28,6 +28,46 @@ class GenCadLoader:
             sectionName = line[1:-1]
             if sectionName in self.sectionsLineNumbers or (sectionName:=sectionName[3:]) in self.sectionsLineNumbers:
                 self.sectionsLineNumbers[sectionName].append(i)
+    
+    def _getBoardDimensions(self, fileLines:str, boardInstance:board.Board):
+        boardOutlineRange = self._calculateRange('BOARD')
+        bottomLeftPoint = geometryObjects.Point(float('Inf'), float('Inf'))
+        topRightPoint = geometryObjects.Point(float('-Inf'), float('-Inf'))
+        handleShape = {'LINE':self._getLineFromLINE, 'ARC': self._getArcFromARC}
+        shapes = []
+
+        for i in boardOutlineRange:
+            if len(fileLines[i]) > 2:
+                keyWord, *line  = fileLines[i].replace('\n', '').split(' ')
+                if keyWord == 'ARTWORK':
+                    break
+                shape, bottomLeftPoint, topRightPoint = handleShape[keyWord](line, bottomLeftPoint, topRightPoint)
+                shapes.append(shape)
+        
+        boardInstance.setOutlines(shapes)
+        boardInstance.setArea(bottomLeftPoint, topRightPoint)
+                
+    
+    def _getLineFromLINE(self, fileLine:list[str], bottomLeftPoint:geometryObjects.Point, topRightPoint:geometryObjects.Point) -> tuple[geometryObjects.Line, geometryObjects.Point, geometryObjects.Point]:
+        _, xStart, yStart, xEnd, yEnd = [geometryObjects.floatOrNone(val) for val in fileLine]
+        startPoint = geometryObjects.Point(xStart, yStart)
+        endPoint = geometryObjects.Point(xEnd, yEnd)
+
+        for point in [startPoint, endPoint]:
+            bottomLeftPoint, topRightPoint = geometryObjects.Point.minXY_maxXYCoords(bottomLeftPoint, topRightPoint, point)
+        
+        return geometryObjects.Line(startPoint, endPoint), bottomLeftPoint, topRightPoint
+    
+    def _getArcFromARC(self, fileLine:str, bottomLeftPoint:geometryObjects.Point, topRightPoint:geometryObjects.Point) -> tuple[geometryObjects.Arc, geometryObjects.Point, geometryObjects.Point]:
+        _, xStart, yStart, xEnd, yEnd, xCenter, yCenter = [geometryObjects.floatOrNone(val) for val in fileLine.split(' ')]
+        startPoint = geometryObjects.Point(xStart, yStart)
+        endPoint = geometryObjects.Point(xEnd, yEnd)
+        rotationPoint = geometryObjects.Point(xCenter, yCenter)
+
+        for point in [startPoint, endPoint, rotationPoint]:
+            bottomLeftPoint, topRightPoint = geometryObjects.Point.minXY_maxXYCoords(bottomLeftPoint, topRightPoint, point)
+        
+        return geometryObjects.Arc(startPoint, endPoint, rotationPoint), bottomLeftPoint, topRightPoint
                 
     def _calculateRange(self, sectionName:str) -> range:
         return range(self.sectionsLineNumbers[sectionName][0], self.sectionsLineNumbers[sectionName][1])
