8be00a1f6f4d5087f14820d0b12fb7e433231f56
Unvire <125924929+Unvire@users.noreply.github.com>
2024-03-25 19:56:39 +0100

    Adds test for _getLineFromLINE

--- Tests/test_gencadLoader.py
+++ Tests/test_gencadLoader.py
@@ -23,6 +23,20 @@ def sectionsRangeTest():
     ]
     return fileLinesMock
 
+@pytest.fixture
+def BouardOutlineTest():
+    fileLinesMock = [
+        '$BOARD\n',
+        'ARC 0.2900811 3.820681 0.250711 3.860051 0.250711 3.820681\n',
+        'LINE 0.2900811 2.129768 0.2506389 2.090325\n',
+        'LINE 0.2506389 2.090325 0.2506389 1.712375\n',
+        'ARC -4.046038 3.859678 -4.08 3.820681 -4.04063 3.820681\n',
+        'ARTWORK artwork1450 SILKSCREEN_TOP\n',
+        'LINE 1967.441 2267.244 2026.496 2267.244\n',
+        '$ENDBOARD\n'
+    ]
+    return fileLinesMock
+
 def test__getSectionsLinesBeginEnd(sectionsRangeTest):
     instance = GenCadLoader()
     instance._getSectionsLinesBeginEnd(sectionsRangeTest)
@@ -33,3 +47,15 @@ def test___calculateRange(sectionsRangeTest):
     instance = GenCadLoader()
     instance._getSectionsLinesBeginEnd(sectionsRangeTest)
     assert instance._calculateRange('SIGNALS') == range(9, 10)
+
+def test__getLineFromLINE():
+    line = ['1967.441', '2267.244', '2026.496', '2267.244']    
+    bottomLeftPoint = geometryObjects.Point(float('Inf'), float('Inf'))
+    topRightPoint = geometryObjects.Point(float('-Inf'), float('-Inf'))
+
+    
+    instance = GenCadLoader()
+    shape, bottomLeftPoint, topRightPoint = instance._getLineFromLINE(line, bottomLeftPoint, topRightPoint)
+    assert shape == geometryObjects.Line(geometryObjects.Point(1967.441, 2267.244), geometryObjects.Point(2026.496, 2267.244))
+    assert bottomLeftPoint == geometryObjects.Point(1967.441, 2267.244)
+    assert topRightPoint == geometryObjects.Point(2026.496, 2267.244)
