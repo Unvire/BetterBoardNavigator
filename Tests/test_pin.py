d89f3e148389f080bf87aeb942775013233ec5b6
Unvire <krzysztof.balcerzak6@gmail.com>
2024-03-28 12:33:52 +0100

    Adds test for calculating center point and dimensions

--- Tests/test_pin.py
+++ Tests/test_pin.py
@@ -9,6 +9,18 @@ def pin1():
     examplePin.setDimensions(2, 4)
     return examplePin
 
+@pytest.fixture
+def pin2():
+    examplePin = pin.Pin('1')
+    examplePin.setPinArea(gobj.Point(-3, -2), gobj.Point(1, 2))
+    return examplePin
+
 def test_calculateArea(pin1):
     pin1.calculateArea()
     assert pin1.pinArea == [gobj.Point(-1, -2), gobj.Point(1, 2)]
+
+def test_calculateCenterDimensionsFromArea(pin2):
+    pin2.calculateCenterDimensionsFromArea()
+    assert pin2.coords == gobj.Point(-1, 0)
+    assert pin2.width == 4
+    assert pin2.height == 4
