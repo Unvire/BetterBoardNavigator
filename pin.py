c17b106dc79e2529504e50d00b13d4044ed82b08
Unvire <krzysztof.balcerzak6@gmail.com>
2024-03-28 13:04:14 +0100

    Calculations are rounded to decimal point specified in Point Class

--- pin.py
+++ pin.py
@@ -45,12 +45,12 @@ class Pin:
     
     def _calculateCenterFromArea(self):
         bottomLeftPoint, topRightPoint = self.pinArea
-        xCenter = (topRightPoint.getX() + bottomLeftPoint.getX()) / 2
-        yCenter = (topRightPoint.getY() + bottomLeftPoint.getY()) / 2
+        xCenter = round((topRightPoint.getX() + bottomLeftPoint.getX()) / 2, gobj.Point.DECIMAL_POINT_PRECISION)
+        yCenter = round((topRightPoint.getY() + bottomLeftPoint.getY()) / 2, gobj.Point.DECIMAL_POINT_PRECISION)
         self.setCoords(gobj.Point(xCenter, yCenter))
     
     def _calculateDimensionsFromArea(self):
         bottomLeftPoint, topRightPoint = self.pinArea
-        width = topRightPoint.getX() - bottomLeftPoint.getX()
-        height = topRightPoint.getY() - bottomLeftPoint.getY()
+        width = round(topRightPoint.getX() - bottomLeftPoint.getX(), gobj.Point.DECIMAL_POINT_PRECISION)
+        height = round(topRightPoint.getY() - bottomLeftPoint.getY(), gobj.Point.DECIMAL_POINT_PRECISION)
         self.setDimensions(width, height)
