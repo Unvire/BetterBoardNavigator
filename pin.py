66713116db2b07507d8f6c219e1cfbafd681467f
Unvire <krzysztof.balcerzak6@gmail.com>
2024-04-09 08:11:50 +0200

    Adds getter for net

--- pin.py
+++ pin.py
@@ -13,30 +13,5 @@ class Pin(abstractShape.Shape):
     def setNet(self, netName:str):
         self.net = netName
     
-    def calculateCenterDimensionsFromArea(self):
-        self._calculateCenterFromArea()
-        self._calculateDimensionsFromArea()
-    
-    def _calculateCenterFromArea(self):
-        bottomLeftPoint, topRightPoint = self.pinArea
-        xCenter = round((topRightPoint.getX() + bottomLeftPoint.getX()) / 2, gobj.Point.DECIMAL_POINT_PRECISION)
-        yCenter = round((topRightPoint.getY() + bottomLeftPoint.getY()) / 2, gobj.Point.DECIMAL_POINT_PRECISION)
-        self.setCoords(gobj.Point(xCenter, yCenter))
-    
-    def _calculateDimensionsFromArea(self):
-        bottomLeftPoint, topRightPoint = self.pinArea
-        width = round(topRightPoint.getX() - bottomLeftPoint.getX(), gobj.Point.DECIMAL_POINT_PRECISION)
-        height = round(topRightPoint.getY() - bottomLeftPoint.getY(), gobj.Point.DECIMAL_POINT_PRECISION)
-        self.setDimensions(width, height)
-    
-    def translateInPlace(self, vector:list[int|float, int|float]):
-        self.coords.translateInPlace(vector)
-        p1, p2 = self.getPinArea()
-        p1.translateInPlace(vector)
-        p2.translateInPlace(vector)
-
-    def rotateInPlace(self, rotationPoint:gobj.Point, angleDeg:int|float):
-        self.coords.rotate(rotationPoint, angleDeg)
-        for point in self.getPinArea():
-            point.rotate(rotationPoint, angleDeg)
-        self._normalizePinArea()
+    def getNet(self) -> str:
+        return self.net
