9f9133aef1de1fde37eeaa49cfe5201663cd5ea5
Unvire <krzysztof.balcerzak6@gmail.com>
2024-04-12 16:10:12 +0200

    Adds method that extracts all paths from files and patterns for regex matching component and eda files

--- Loaders/odbPlusPlusLoader.py
+++ Loaders/odbPlusPlusLoader.py
@@ -1,4 +1,4 @@
-import sys, os, copy
+import sys, os, copy, re
 sys.path.append(os.getcwd())
 import tarfile
 import geometryObjects as gobj
@@ -12,10 +12,24 @@ class ODBPlusPlusLoader():
 
     def loadFile(self, filePath:str):
         self._setFilePath(filePath)
+        self._getFileLinesFromTar()
 
     def _setFilePath(self, filePath:str):
         self.filePath = filePath
 
+    def _getFileLinesFromTar(self):
+        with tarfile.open(self.filePath, 'r') as file:
+            tarPaths = file.getnames()
+        
+        self._getTarPathsToEdaComponents(tarPaths)
+        
+    def _getTarPathsToEdaComponents(self, tarPaths:list[str]) -> list[str]:
+        componentsFilePattern = '^\w+\/steps\/\w+\/layers\/comp_\+_(bot|top)\/components(.Z)?$' # matches comp_+_bot and comp_+_top files both zipped and uzipped
+        edaFilePattern = '^\w+\/steps\/\w+\/eda\/data(.Z)?$' # matches eda path both zipped and unzipped
+        for name in tarPaths:
+            print(name)
+        
+
 if __name__ == '__main__':
     loader = ODBPlusPlusLoader()
-    loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\odb\660891125.tgz')
+    loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\odb\m10.tgz')
