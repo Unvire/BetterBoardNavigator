8aea4b8e72a944f8f784157f3f9c70b5edade03e
Unvire <krzysztof.balcerzak6@gmail.com>
2024-04-22 14:33:55 +0200

    Adds "factory" of file loaders

---
+++ loaderSelector.py
@@ -0,0 +1,18 @@
+import Loaders.camcadLoader
+import Loaders.gencadLoader
+import Loaders.odbPlusPlusLoader
+import board
+
+class LoaderSelector:
+    def __init__(self):
+        self.loadersDict = {
+            '.cad': Loaders.camcadLoader.CamCadLoader,
+            '.gcd': Loaders.gencadLoader.GenCadLoader,
+            '.tgz': Loaders.odbPlusPlusLoader.ODBPlusPlusLoader
+        }
+
+    def loadFile(self, filePath:str) -> board.Board:
+        fileExtension = filePath.split('.')[-1]
+        if fileExtension in self.loadersDict:
+            loader = self.loadersDict[fileExtension]()
+            return loader.loadFile(filePath)
