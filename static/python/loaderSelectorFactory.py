a7632aec516a31058b80503d3da581cd25b341f2
Unvire <krzysztof.balcerzak6@gmail.com>
2024-04-29 13:53:27 +0200

    Adds self test for factory

--- static/python/loaderSelectorFactory.py
+++ static/python/loaderSelectorFactory.py
@@ -17,3 +17,19 @@ class LoaderSelectorFactory:
     def processFile(self, fileLines:list[str]) -> board.Board:
         boardInstance = self.loaderInstance.processFile(fileLines)
         return boardInstance
+
+if __name__ == '__main__':
+    loader = LoaderSelectorFactory('cad')
+    fileLines = loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\lvm Core.cad')
+    _ = loader.processFile(fileLines)
+    print('camcad file loaded')
+
+    loader = LoaderSelectorFactory('gcd')
+    fileLines = loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\jaguar REV.GCD')
+    _ = loader.processFile(fileLines)
+    print('gencad file loaded')
+
+    loader = LoaderSelectorFactory('tgz')
+    fileLines = loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\odb\DEL2114.tgz')
+    _ = loader.processFile(fileLines)
+    print('odb++ file loaded')
