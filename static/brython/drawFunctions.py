5628a229c4f1a45ff302fdc858cde597987de25e
Unvire <krzysztof.balcerzak6@gmail.com>
2024-04-30 12:24:50 +0200

    Changes names from (...)Point to XY to make clearer that (x, y) tuple is expected

--- static/brython/drawFunctions.py
+++ static/brython/drawFunctions.py
@@ -1,16 +1,16 @@
 from browser import window
 
-def drawLine(context:'canvas.getContext()', startPointTuple:tuple[int, int], endPointTuple:tuple[int, int], color:str, lineWidth:int):
-    x0, y0 = startPointTuple
-    x1, y1 = endPointTuple
+def drawLine(context:'canvas.getContext()', startXY:tuple[int, int], endXY:tuple[int, int], color:str, lineWidth:int):
+    x0, y0 = startXY
+    x1, y1 = endXY
     context.beginPath()
     context.moveTo(x0, y0)
     context.lineTo(x1, y1)
     _setCommonParameters(context, color, lineWidth)
 
-def drawRectangle(context:'canvas.getContext()', bottomLeftPoint:tuple[int, int], topRightPoint:tuple[int, int], color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
-    x0, y0 = bottomLeftPoint
-    x1, y1 = topRightPoint
+def drawRectangle(context:'canvas.getContext()', bottomLeftXY:tuple[int, int], topRightXY:tuple[int, int], color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
+    x0, y0 = bottomLeftXY
+    x1, y1 = topRightXY
     context.beginPath()
     width, height = x1 - x0, y1 - y0
     context.rect(x0, y0, width, height)
@@ -18,11 +18,11 @@ def drawRectangle(context:'canvas.getContext()', bottomLeftPoint:tuple[int, int]
     context.lineWidth = lineWidth
     _setCommonParameters(context, color, lineWidth, isFill, fillColor)
 
-def drawCircle(context:'canvas.getContext()', centerPoint:tuple[int, int], radius:float|int, color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
-    drawArc(context, centerPoint, radius, 0, 2 * window.Math.PI, color, lineWidth, isFill, fillColor)
+def drawCircle(context:'canvas.getContext()', centerXY:tuple[int, int], radius:float|int, color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
+    drawArc(context, centerXY, radius, 0, 2 * window.Math.PI, color, lineWidth, isFill, fillColor)
 
-def drawArc(context:'canvas.getContext()', centerPoint:tuple[int, int], radius:float|int, startAngleRad:float|int, endAngleRad:float|int, color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
-    x0, y0 = centerPoint
+def drawArc(context:'canvas.getContext()', centerXY:tuple[int, int], radius:float|int, startAngleRad:float|int, endAngleRad:float|int, color:str, lineWidth:int, isFill:bool=False, fillColor:str='red'):
+    x0, y0 = centerXY
     context.beginPath()
     context.arc(x0, y0, radius, startAngleRad, endAngleRad)
     _setCommonParameters(context, color, lineWidth, isFill, fillColor)
