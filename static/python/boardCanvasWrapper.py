dc393036145ea6068409af407ce618a72985a919
Unvire <krzysztof.balcerzak6@gmail.com>
2024-05-07 12:47:50 +0200

    Adds method for loading and setting base board

--- static/python/boardCanvasWrapper.py
+++ static/python/boardCanvasWrapper.py
@@ -9,17 +9,21 @@ class BoardCanvasWrapper():
         self.height = height
         self.baseBoard = None
 
-    def loadBaseBoard(self, filePath:str):
+    def loadAndSetBaseBoard(self, filePath:str):
+        boardInstance = self._loadBaseBoard(filePath)
+        self._setBaseBoard(boardInstance)
+    
+    def normalizeBoard(self):
+        pass
+
+    def _loadBaseBoard(self, filePath:str) -> board.Board:
         fileExtension  = filePath.split('.')[-1]
         loader = loaderSelectorFactory.LoaderSelectorFactory(fileExtension)
         fileLines = loader.loadFile(filePath)
         return loader.processFileLines(fileLines)
 
-    def setBaseBoard(self, boardInstace:board.Board):
+    def _setBaseBoard(self, boardInstace:board.Board):
         self.baseBoard = boardInstace
-
-    def normalizeBoard(self):
-        pass
 
     def _calculateBaseScale(self, boardArea:tuple[gobj.Point, gobj.Point]):
         bottomLeftPoint, topRightPoint = boardArea
