65a8efaa34af959ff0c20a1b05e31cc7a93bb219
Unvire <krzysztof.balcerzak6@gmail.com>
2024-05-09 15:17:07 +0200

    Adds function for drawing arcs

--- static/python/pygameDrawBoard.py
+++ static/python/pygameDrawBoard.py
@@ -11,6 +11,13 @@ def drawLine(surface:pygame.Surface, color:tuple[int, int, int], lineInstance:go
     startPoint, endPoint = lineInstance.getPoints()
     pygame.draw.line(surface, color, startPoint.getXY(), endPoint.getXY())
 
+def drawArc(surface:pygame.Surface, color:tuple[int, int, int], arcInstance:gobj.Arc, width:int=1):
+    startPoint, endPoint = arcInstance.getPoints()[:2]
+    x0, y0, = startPoint.getXY()
+    x1, y1 = endPoint.getXY()
+    endAngle, startAngle = arcInstance.getAsCenterRadiusAngles()[-2:] # y axis is mirrored so angles are swapped    
+    pygame.draw.arc(surface, color, (x0, y0, x1 - x0, y1 - y0), startAngle, endAngle, width)
+
 if __name__ == '__main__':
     WIDTH, HEIGHT = 1200, 700
     FPS = 60
@@ -28,6 +35,8 @@ if __name__ == '__main__':
     clock = pygame.time.Clock()
     
     lineInstance = gobj.Line(gobj.Point(100, 100), gobj.Point(1000, 630))
+    arcInstance = gobj.Arc(gobj.Point(200, 200), gobj.Point(400, 400) ,gobj.Point(400, 200))
+
     run = True
     while run:
         clock.tick(FPS)
@@ -45,7 +54,7 @@ if __name__ == '__main__':
             elif event.type == pygame.KEYDOWN:
                pass
             
-        drawLine(boardLayer, (255, 255, 255), lineInstance)
+        drawArc(boardLayer, (255, 255, 255), arcInstance)
 
         ## display image
         drawBoardLayer(WIN, [boardLayer])
