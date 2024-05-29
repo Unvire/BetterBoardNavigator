63124cf7dc9f76c03e04e2acac2c2648dcc474df
Unvire <krzysztof.balcerzak6@gmail.com>
2024-05-29 15:33:13 +0200

    Adds test for loading schematic file, processing it by schematicFileLoader and printing fileLines

---
+++ static/js/openAndLoadFile.js
@@ -0,0 +1,11 @@
+async function openAndLoadCadFile(pyodide, file) {
+    const reader = new FileReader(); 
+
+    reader.onload = async (event) => {
+        const fileContent = event.target.result;
+        const fileName = `/${file.name}`
+        // copy file to virtual memory
+        pyodide.FS.writeFile(fileName, new Uint8Array(fileContent));
+    };
+    reader.readAsArrayBuffer(file);
+}
