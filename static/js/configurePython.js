f23fd6cd8320d387f7e0560e643fd1b655bf964f
Unvire <125924929+Unvire@users.noreply.github.com>
2024-05-31 14:50:40 +0200

    Changes for loop to foreach, adds method for setting u[ python path and changes name of the file

--- static/js/configurePython.js
+++ static/js/configurePython.js
@@ -1,3 +1,16 @@
+async function configurePythonPath(pyodide){
+    await pyodide.runPythonAsync(`
+                import sys
+                sys.path.append("/")
+    `);
+}
+
+async function loadPygame(pyodide){
+    await pyodide.loadPackage("micropip");
+    const micropip = pyodide.pyimport("micropip");
+    await micropip.install("pygame-ce");
+}
+
 async function loadLocalModules(pyodide) {
     async function copyModuleToVirtualMemory(pyodide, moduleName){
         var response = await fetch(`/static/python/${moduleName}.py`);
@@ -5,19 +18,11 @@ async function loadLocalModules(pyodide) {
         pyodide.FS.writeFile(`/${moduleName}.py`, moduleCode);
     }
 
-
     const modulesList = ['geometryObjects', 'abstractShape', 'pin', 'component', 'board', 'unlzw3', 
                           'camcadLoader', 'gencadLoader', 'odbPlusPlusLoader', 'loaderSelectorFactory',
                           'boardWrapper', 'pygameDrawBoard']
-
-    for (let i = 0; i < modulesList.length; i++) {
-        var moduleName = modulesList[i]
+    
+    modulesList.forEach(async (moduleName) => {
         await copyModuleToVirtualMemory(pyodide, moduleName)
-    }
-}
-
-async function loadPygame(pyodide){
-    await pyodide.loadPackage("micropip");
-    const micropip = pyodide.pyimport("micropip");
-    await micropip.install("pygame-ce");
+    });
 }
