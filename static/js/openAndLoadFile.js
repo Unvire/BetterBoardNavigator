f48346874ef86b7d15eb966d06a8f60ab5b3051b
Unvire <125924929+Unvire@users.noreply.github.com>
2024-06-02 13:27:01 +0200

    Removes button list files. File is processed after loading

--- static/js/openAndLoadFile.js
+++ static/js/openAndLoadFile.js
@@ -1,11 +1,21 @@
 async function openAndLoadCadFile(pyodide, file) {
+    var fileName = `/${file.name}`
     const reader = new FileReader(); 
 
     reader.onload = async (event) => {
         const fileContent = event.target.result;
-        const fileName = `/${file.name}`
-        
         pyodide.FS.writeFile(fileName, new Uint8Array(fileContent));
+
+        // process .cad, .gcd and .tgz files
+        await pyodide.runPythonAsync(`
+            from loaderSelectorFactory import LoaderSelectorFactory
+
+            loader = LoaderSelectorFactory("/${fileName}")
+            fileLines = loader.loadFile("/${fileName}")
+            print(fileLines)
+            board = loader.processFileLines(fileLines)
+            print(board)
+        `);
     };
     reader.readAsArrayBuffer(file);
 }
