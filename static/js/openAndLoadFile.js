cf763a702d304b377b9a4e8f20c7febb21628211
Unvire <125924929+Unvire@users.noreply.github.com>
2024-06-02 13:55:00 +0200

    Adds function to clear previous loaded file into FS

--- static/js/openAndLoadFile.js
+++ static/js/openAndLoadFile.js
@@ -5,17 +5,24 @@ async function openAndLoadCadFile(pyodide, file) {
     reader.onload = async (event) => {
         const fileContent = event.target.result;
         pyodide.FS.writeFile(fileName, new Uint8Array(fileContent));
-
+        
         // process .cad, .gcd and .tgz files
         await pyodide.runPythonAsync(`
             from loaderSelectorFactory import LoaderSelectorFactory
 
-            loader = LoaderSelectorFactory("/${fileName}")
-            fileLines = loader.loadFile("/${fileName}")
-            print(fileLines)
-            board = loader.processFileLines(fileLines)
-            print(board)
+            #loader = LoaderSelectorFactory("/${fileName}")
+            #fileLines = loader.loadFile("/${fileName}")
+            #print(fileLines)
+            #board = loader.processFileLines(fileLines)
+            #print(board)
         `);
     };
     reader.readAsArrayBuffer(file);
+}
+
+async function removePreviousFileFromFS(pyodide, fileName){
+    const pydodideFiles = pyodide.FS.readdir('/');
+    if (pydodideFiles.includes(fileName)){
+        pyodide.FS.unlink(`/${fileName}`);
+    }
 }
