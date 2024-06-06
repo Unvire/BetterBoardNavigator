cdd041d9e4a106e47b89ec1b2b3292f8e6875e7b
Unvire <125924929+Unvire@users.noreply.github.com>
2024-06-06 20:40:50 +0200

    moves mouse events and resize events to event file. fixes resizing

--- static/js/events.js
+++ static/js/events.js
@@ -1,3 +1,38 @@
+async function windowResizeEvent(event){
+    let RESCALE_AFTER_MS = 3;
+    clearTimeout(resizeTimeout);
+    resizeTimeout = setTimeout(resizeCanvas, RESCALE_AFTER_MS);
+};
+
+function mouseDownEvent(event){
+    const x = event.offsetX; 
+    const y = event.offsetY;
+    isMousePressed = true;
+    isMouseClickedFirstTime = true;
+};
+
+function mouseUpEvent(){
+    isMousePressed = false;
+};
+
+async function mouseMoveEvent(event){
+    if (isMousePressed){
+        if (!isMouseClickedFirstTime){
+            const x = event.movementX; 
+            const y = event.movementY;
+
+            pyodide.runPythonAsync(`
+                if engine:
+                    deltaVector = [int('${x}'), int('${y}')]
+                    engine.moveBoardInterface(SURFACE, deltaVector)
+                    pygame.display.flip()
+            `);
+        } else {
+            isMouseClickedFirstTime = false;
+        }
+    }
+};
+
 async function loadFileEvent(event){
     const file = event.target.files[0];
     if (file) {
