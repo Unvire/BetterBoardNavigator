84262b185d256b229bafb691930be3781e57ff1c
Unvire <krzysztof.balcerzak6@gmail.com>
2024-06-11 10:11:29 +0200

    Adds test for getComponents

--- static/python/Tests/test_pygameDrawBoard.py
+++ static/python/Tests/test_pygameDrawBoard.py
@@ -1,5 +1,6 @@
 import pytest
 from pygameDrawBoard import DrawBoardEngine
+import board
 
 def test__calculateOffsetVectorForScaledSurface():
     engine = DrawBoardEngine(1200, 700)
@@ -52,3 +53,21 @@ def test__calculateOffsetVectorForScaledSurface():
     engine.offsetVector = newOffset
     newOffset = engine._calculateOffsetVectorForScaledSurface(zoomingPoint, 1 / 1.3)
     assert newOffset == (-50, -50)
+
+def test_getComponents():
+    boardInstance = board.Board()
+    boardInstance.components = {'HOLE': '', 
+                                'C10': '',
+                                'C11': '',
+                                'C13': '',
+                                'C2': '',
+                                'C20': '',
+                                'R7': '',
+                                'R19': '',
+                                'R1': '',
+                                'P2_U7_COMP':''}
+    engine = DrawBoardEngine(1200, 700)
+    engine.boardData = boardInstance
+
+    expected = ['C2', 'C10', 'C11', 'C13', 'C20', 'P2_U7_COMP', 'R1', 'R7', 'R19', 'HOLE']
+    assert engine.getComponents() == expected
