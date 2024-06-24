cd75e76df67e46c9a951379aae3bd808fc83f64b
Unvire <krzysztof.balcerzak6@gmail.com>
2024-06-24 13:03:21 +0200

    Adds sideHandler for dealing with side operations

---
+++ static/js/sideHandler.js
@@ -0,0 +1,31 @@
+class SideHandler{
+    constructor(){
+        this.isSideBottom = true;
+        this.sideMap = {true: "B", false: "T"};
+    }
+
+    currentSide(){
+        return this.sideMap[this.isSideBottom];
+    }
+
+    changeSide(){            
+        this.isSideBottom = !(this.isSideBottom);
+        currentSideSpan.innerText = this.currentSide();
+        return this.currentSide();
+    }
+
+    getSideOfComponent(componentName){
+        pyodide.runPython(`
+            componentSide = engine.getSideOfComponent('${componentName}')
+        `);
+        return pyodide.globals.get("componentSide");
+    }
+
+    setComponentSideAsCurrentSide(componentName){
+        const componentSide = this.getSideOfComponent(componentName);
+        if (componentSide != this.currentSide()){
+            return this.changeSide();
+        }
+        return this.currentSide();
+    }
+}
