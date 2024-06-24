8daa6a1d577aa7742bb86e0c5f381ebe2114a8b9
Unvire <krzysztof.balcerzak6@gmail.com>
2024-06-24 15:46:56 +0200

    Adds a dataclass with all variables/instances used over the adapters

---
+++ static/js/globalInstancesMap.js
@@ -0,0 +1,20 @@
+class GlobalVariablesMap{
+    constructor(){
+        this.modalSubmit = null;
+        this.textModalSubmitButton = null;
+        this.textModalInput = null;
+        this.modalParagraph = null;
+        this.allComponentsList = null;
+        this.markedComponentsList = null;
+        this.pinoutTable = null;
+        this.netsTreeview = null;
+        this.clickedComponentSpanList = null;
+        this.sideHandler = null;
+        this.canvas = null;
+        this.canvasParents = null;
+        this.commonPrefixSpan = null;
+        this.selectedComponentSpan = null;
+    }
+
+
+}
