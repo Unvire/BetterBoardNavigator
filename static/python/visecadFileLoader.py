fac5d3de1c06ac723942028c3c79ce7e6d5a8e02
Unvire <krzysztof.balcerzak6@gmail.com>
2024-07-02 15:01:50 +0200

    xml file is processed preserving current convention (1. get file lines, 2. process them)

--- static/python/visecadFileLoader.py
+++ static/python/visecadFileLoader.py
@@ -1,4 +1,4 @@
-import xml.etree.ElementTree as et
+import xml.etree.ElementTree as ET
 from zipfile import ZipFile
 import geometryObjects as gobj
 import component as comp
@@ -10,17 +10,27 @@ class VisecadLoader():
         self.boardData = board.Board()
 
     def loadFile(self, filePath:str) -> list[str]:
-        with ZipFile(filePath, 'r') as zippedFile:
-            xmlFile = zippedFile.namelist()[0]
-            with zippedFile.open(xmlFile) as file:
-                fileLines = file.readlines()
+        fileLines = self._getFileLines(filePath)
         return fileLines
     
     def processFileLines(self, fileLines:list[str]) -> board.Board:
+        root = self._parseXMLFromFileLines(fileLines)
+
+
         return self.boardData
+    
+    def _getFileLines(self, filePath:str) -> list[str]:
+        fileLines = []
+        with ZipFile(filePath, 'r') as zippedFile:
+            xmlFileName = zippedFile.namelist()[0]
+            with zippedFile.open(xmlFileName) as xmlFile:
+                fileLines = [line.decode('utf-8') for line in xmlFile.readlines()]
+        return fileLines
+
+    def _parseXMLFromFileLines(self, fileLines:list[str]) -> 'xml.etree.ElementTree':
+        return ET.fromstring(''.join(fileLines))
 
 if __name__ == '__main__':
     loader = VisecadLoader()
-    fileLines = loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\ccz\15017460_02.ccz')
-    print(fileLines)
+    fileLines = loader.loadFile(r'C:\Python 3.11.1\Compiled\Board Navigator\Schematic\ccz\!15017460_02.ccz')
     loader.processFileLines(fileLines)
